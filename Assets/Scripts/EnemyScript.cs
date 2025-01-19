c9c44d82f47ff1fd7f631d1aa96be89c95f4b0cf
Jackie Williams <jackfastcool@gmail.com>
2025-01-19 14:19:28 +0000

    Enemy Mode 2 (Follow player directly) Completed

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    Camera cam;
+
+    [SerializeField] GameObject followTarget;
+    [SerializeField] Rigidbody2D rigidbody1;
+    [SerializeField] int enemyMode; //1 - random direction once hit wall | 2 - follow player directly
+
+    // Awake is called when the object is created
+    void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyMode == 1)
+        {
+
+        }
+        else if (enemyMode == 2)
+        {
+            Vector3 followPos = followTarget.transform.position;
+            rigidbody1.SetRotation(Quaternion.LookRotation(Vector3.forward, followPos - transform.position));
+        }
+    }
+
+    
+}
